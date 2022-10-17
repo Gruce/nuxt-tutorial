3213ff9e8ba74caec514f41ebc7cc23152efbc52
zal3 <zainabamjedlatef31@gmail.com>
2022-10-18 00:09:02 +0300

    لا

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,9 @@
 <template>
     <div>
-        <!-- <div text="center 3xl green">My Heading</div> -->
         <Header/>
-        <!-- <hr class="w-lg"> -->
         <div>
             <slot />
         </div>
-        <!-- <hr class="w-222px"> -->
         <div text="center 2xl green">My footer</div>
     </div>
 </template>
