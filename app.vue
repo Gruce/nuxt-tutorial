ad7d66c0450f2ca2cdb359f36a41db4ba16ce35f
Mohammed Jawad <jawad6686@gmail.com>
2022-10-18 22:08:56 +0300

    add

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
   <NuxtLayout>
     <NuxtPage />
-    <NameAndAge />
-    <div class="flex justify-around" mt="44" text="xl ">   <!--** Who is beter ? **-->
-      <NuxtLink href="/">To Hero Page</NuxtLink>          <!--<a href="/">To Hero Page</a>-->
-      <NuxtLink href="/intro">To Intro Page</NuxtLink>    <!--<a href="/intro">To Intro Page</a>-->
-    </div>
   </NuxtLayout>
 </template>
