ba8bbc1799af4133ee9b578a0af7e09c9e5ecccf
Maryam Walid <maryam.walid.abdaljalil@gmail.com>
2022-10-21 21:14:47 +0300

    add

--- components/Task.vue
+++ components/Task.vue
@@ -2,7 +2,7 @@
 
 <div  flex="gap-4">
                     <span m-5>{{taskIndex+1}}</span>
-                    <span border-2 m-5  >{{task.title}}</span>
+                    <span border-2 m-5>{{task.title}}</span>
                     <span border-2 m-5>{{task.descrption}}</span>
                     <span border-2 m-5>{{task.url}}</span>
                    
