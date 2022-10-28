612a3e4b5f4f277197f4217855318ba4c3cbfb5a
Sky <jafarsky@users.noreply.github.com>
2022-10-28 21:43:52 +0300

    some edit

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,11 @@
         <template #tab-1>CONTENT 1</template>
         <template #tab-2>CONTENT 2</template>
         <template #tab-3>CONTENT 3</template>
-        
     </Tabs>
 </template>
 
 <script setup>
-const tabs = ['Tab 1', 'Tab 2', 'Tab 3']
-
-const doSomething = () => console.log('Do Something')
+    const tabs = ['Tab 1', 'Tab 2', 'Tab 3'];
+    
+    const doSomething = () => console.log('Do Something');
 </script>
